9382de6ec813eb7cdf4a76873e9e80caa68f9dbc
Donald Yusuf <donalddyusuf@gmail.com>
2024-08-20 05:35:15 -0700

    Merged Conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import './App.css';
@@ -49,37 +49,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./Pages/Home"
-import Menu from "./Pages/Menu"
-import './App.css'
-import { CardProvider } from "./Context/CardContext"
-import { CartProvider } from "react-use-cart"
-import CheckoutWrapper from "./Components/CheckoutWrapper"
-import Contact from "./Pages/Contact"
-import Navbar from "./Components/Navbar"
-import LandingPage from "./Components/LandingPage"
-const App = () => {
-  return (
-    <>
-    <CardProvider>
-      <CartProvider>
-    <BrowserRouter>
-    
-    <Routes>
-      <Route path="/" element={<LandingPage/>}/>
-      <Route path="/home" element={ <Home/>}/>
-      <Route path="/menu" element={<Menu/>}/>
-    <Route path="/checkout" element={<CheckoutWrapper/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      
-    </Routes>
-    </BrowserRouter>
-    </CartProvider>
-    </CardProvider>
-    </>
-  )
-}
-export default App
->>>>>>> 814157c7b21617e8c5d0ca4def52305bd3730122
